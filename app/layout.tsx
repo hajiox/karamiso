13a7933409d4371a069470aeb2cf949451470788
hajiox <ts@aizu-tv.com>
2025-07-26 10:04:31 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import Script from "next/script"
 import "./globals.css"
-import AnalyticsWrapper from "./components/analytics-wrapper"
+import AnalyticsWrapper from "@/components/analytics-wrapper"
 
 const inter = Inter({ subsets: ["latin"] })
 
